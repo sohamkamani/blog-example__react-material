d6719301fe5253589d69b670f94d8056ee764c6e
sohamkamani <soham_chetan_kamani@mckinsey.com>
2015-12-28 15:12:54 +0530

    bootstrap

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+let App = React.createClass({
+  render : function(){
+    return (
+      <div>
+        App
+      </div>
+    );
+  }
+});
+
+module.exports = App;
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       exclude: /(node_modules|bower_components)/,
       loader: 'babel',
       query: {
-        presets: ['es2015']
+        presets: ['es2015', 'react']
       }
     }, {
       test: /\.scss$/,
